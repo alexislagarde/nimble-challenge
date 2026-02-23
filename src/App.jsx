9e5e51b84364d40d8ca20bad6b4bf23d0f54f32e
alexis <alexislagarde@hotmail.com>
2026-02-23 19:19:12 -0300

    add tabla de candidatos

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import "./App.css";
+import { fetchCandidateByEmail } from "./Api/handleApi";
 
 function App() {
-  return (
-    <div>
-      <h1>Nimble Gravity Challenge</h1>
-    </div>
-  );
+const { candidate } = fetchCandidateByEmail("alexislagarde@hotmail.com")
+
+return (
+<div className="App">
+ <h1>Nimble Gravity Challenge</h1>
+  <div className="card-candidates">
+    <table className="candidatesTable">
+      <thead>
+        <tr>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Email</th>
+          <th>Candidate ID</th>
+          <th>Application ID</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {candidate && (
+          <tr>
+            <td>{candidate.firstName}</td>
+            <td>{candidate.lastName}</td>
+            <td>{candidate.email}</td>
+            <td>{candidate.candidateId}</td>
+            <td>{candidate.applicationId}</td>
+          </tr>
+        )}
+      </tbody>
+    </table>
+  </div>
+</div>
+
+
+);
 }
 
 export default App;
